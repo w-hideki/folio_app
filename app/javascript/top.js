5e9291eb18b593a7b43d5808ea266a2ea8672ca8
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-12-18 09:31:51 +0900

    レスポンシブ化完了

--- app/javascript/top.js
+++ app/javascript/top.js
@@ -53,5 +53,20 @@ $(function(){
   });
 });
 
+$(function() {
+  /* SP menu */
+  function toggleNav() {
+      var body = document.body;
+      var hamburger = document.getElementById('nav_btn');
+      var blackBg = document.getElementById('nav_bg');
+      hamburger.addEventListener('click', function() {
+          body.classList.toggle('nav_open'); //メニュークリックでnav-openというクラスがbodyに付与
+      });
+      blackBg.addEventListener('click', function() {
+          body.classList.remove('nav_open'); //もう一度クリックで解除
+      });
+  }
+  toggleNav();
+});
 
 
